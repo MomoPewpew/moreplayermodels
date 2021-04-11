31fdbf134f1c49a4349ff0ae3a2be723bf952c74
marijntepas <marijntepas@gmail.com>
2021-04-11 21:20:45 +0200

    Updated client.layer

--- src/main/java/noppes/mpm/client/layer/LayerEyes.java
+++ src/main/java/noppes/mpm/client/layer/LayerEyes.java
@@ -1,6 +1,12 @@
 package noppes.mpm.client.layer;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
@@ -22,13 +28,13 @@ public class LayerEyes extends LayerInterface{
 		model.bipedHead.postRender(0.0625F);
 		GlStateManager.scale(par7, par7, -par7);
 		GlStateManager.translate(0, (playerdata.eyes.type == 1?1:2) - playerdata.eyes.eyePos, 0);
-		
+
         GlStateManager.disableTexture2D();
-        int i = (int) (player.getBrightnessForRender(par7));
+        int i = this.player.getBrightnessForRender();
         int j = i % 65536;
         int k = i / 65536;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
-        
+
         drawBrows();
         drawLeft();
         drawRight();
@@ -36,11 +42,11 @@ public class LayerEyes extends LayerInterface{
 		GlStateManager.popMatrix();
         GlStateManager.enableTexture2D();
 	}
-	
+
 	private void drawLeft(){
 	    if(playerdata.eyes.pattern == 2)
 		       return;
-		drawRect(3, -5, 1, -4, 0xF6F6F6, 4.01, false);				
+		drawRect(3, -5, 1, -4, 0xF6F6F6, 4.01, false);
 		drawRect(2, -5, 1, -4, playerdata.eyes.color, 4.011, playerdata.eyes.type == 1);
 		if(playerdata.eyes.glint && player.isEntityAlive()){
 			drawRect(1.5, -4.9, 1.9, -4.5, 0xFFFFFFFF, 4.012, false);
@@ -48,9 +54,9 @@ public class LayerEyes extends LayerInterface{
 		if(playerdata.eyes.type == 1){
 			drawRect(3, -4, 1, -3, 0xFFFFFF, 4.01, true);
 			drawRect(2, -4, 1, -3, playerdata.eyes.color, 4.011, false);
-		}	    
+		}
 	}
-	
+
 	private void drawRight(){
 	    if(playerdata.eyes.pattern == 1)
 		       return;
@@ -64,7 +70,7 @@ public class LayerEyes extends LayerInterface{
 				drawRect(-2, -4, -1, -3, playerdata.eyes.color, 4.011, false);
 			}
 	}
-	
+
 	private void drawBrows(){
 		float offsetY = 0;
 		if(playerdata.eyes.blinkStart > 0 && player.isEntityAlive()){
@@ -85,7 +91,7 @@ public class LayerEyes extends LayerInterface{
             drawRect(1, -5 + offsetY, 3, -5 - thickness + offsetY, playerdata.eyes.browColor, 4.014, false);
        }
 	}
-	
+
 	public void drawRect(double x, double y, double x2, double y2, int color, double z, boolean darken)
     {
 		double j1;
@@ -112,7 +118,7 @@ public class LayerEyes extends LayerInterface{
         	f2 *= 0.96f;
         	f3 *= 0.96f;
         }
-        VertexBuffer tessellator = Tessellator.getInstance().getBuffer();
+        BufferBuilder tessellator = Tessellator.getInstance().getBuffer();
         GlStateManager.color(1, 1, 1, 1);
         tessellator.begin(7, DefaultVertexFormats.POSITION_COLOR);
         tessellator.pos((double)x, (double)y, z).color(f1, f2, f3, 1).endVertex();
@@ -124,7 +130,7 @@ public class LayerEyes extends LayerInterface{
 
 	@Override
 	public void rotate(float par1, float par2, float par3, float par4, float par5, float par6) {
-		
+
 	}
 
 }
